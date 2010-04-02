f019ced64516f5150bf99a01d7ae08762cb806a1
Johannes Waldmann <waldmann@octopus.home>
2010-04-02 21:47:46 +0200

    add

---
+++ Satchmo/Simple.hs
@@ -0,0 +1,30 @@
+{-# language GeneralizedNewtypeDeriving #-}
+
+module Satchmo.Simple where
+
+import Satchmo.MonadSAT
+import Satchmo.Data
+
+import Control.Monad.State
+
+data Accu = Accu { next :: ! Int
+                 , pool :: [ Clause ]
+                 }
+
+start = Accu { next = 0, pool = [] }
+
+sat (SAT m) = flip evalState start
+            $ do x <- m; a <- get ; return (x, a) 
+
+newtype SAT a = SAT { unsat :: State Accu a } 
+    deriving ( Functor, Monad )
+
+instance MonadSAT SAT where
+    fresh = SAT $ do 
+          a <- get ; let n = succ $ next a 
+          put $ a { next = n } ; return $ Literal n
+    emit c = SAT $ do
+          modify $ \ a -> a { pool = c : pool a }
+
+
+        
