1d496442f071d092e81b605ec8052b1d5f90a0c8
Johannes Waldmann <waldmann@imn.htwk-leipzig.de>
2012-03-05 13:23:16 +0100

    op

--- Satchmo/Unary/Op/Fixed.hs
+++ Satchmo/Unary/Op/Fixed.hs
@@ -1,2 +1,32 @@
-module Satchmo.Unary.Op.Fixed where
+module Satchmo.Unary.Op.Fixed 
 
+( module Satchmo.Unary.Op.Common 
+, plus
+)       
+       
+where
+
+import Prelude hiding ( not, and, or )
+import qualified Prelude
+
+import Satchmo.Boolean
+import   Satchmo.Unary.Op.Common
+import   Satchmo.Unary.Data
+
+import Control.Monad ( forM, when )
+import qualified Data.Map as M
+
+-- | Unary addition. Output bit length is sum of input bit lengths.
+plus :: MonadSAT m => Number -> Number -> m Number
+plus a b = do
+    let width = Prelude.max ( length $ bits a ) 
+                            ( length $ bits b ) 
+    t <- Satchmo.Boolean.constant True
+    pairs <- forM ( zip [0..] $ t : bits a ) $ \ ( i,x) -> 
+             forM ( zip [0..] $ t : bits b ) $ \ ( j,y) -> 
+             do z <- and [x,y] ; return (i+j, [z])
+    cs <- forM ( tail $ map snd $ M.toAscList $ M.fromListWith (++) $ concat pairs ) or
+    let ( pre, post ) = splitAt width cs
+    when ( Prelude.not $ null post ) $ assert [ not $ head post ]
+    return $ make pre
+    
--- Satchmo/Unary/Op/Flexible.hs
+++ Satchmo/Unary/Op/Flexible.hs
@@ -1,8 +1,29 @@
 module Satchmo.Unary.Op.Flexible 
        
-( module Satchmo.Unary.Op.Common )       
+( module Satchmo.Unary.Op.Common 
+, plus
+)       
        
 where
 
-import   Satchmo.Unary.Op.Common
+import Prelude hiding ( not, and, or )
+import qualified Prelude
 
+import Satchmo.Boolean
+import   Satchmo.Unary.Data
+import Satchmo.Unary.Op.Common 
+
+import Control.Monad ( forM )
+import qualified Data.Map as M
+
+-- | Unary addition. Output bit length is sum of input bit lengths.
+plus :: MonadSAT m => Number -> Number -> m Number
+plus a b = do
+    t <- Satchmo.Boolean.constant True
+    pairs <- forM ( zip [0..] $ t : bits a ) $ \ ( i,x) -> 
+             forM ( zip [0..] $ t : bits b ) $ \ ( j,y) -> 
+             do z <- and [x,y] ; return (i+j, [z])
+    cs <- forM ( map snd $ tail $ M.toAscList $ M.fromListWith (++) $ concat pairs ) or
+    return $ make cs
+
+    
