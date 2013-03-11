54962e7647ae81880be9676a23454611ad49f420
Johannes Waldmann <waldmann@imn.htwk-leipzig.de>
2013-03-11 16:22:34 +0100

    repair implementation

--- examples/Wilkie.hs
+++ examples/Wilkie.hs
@@ -10,39 +10,51 @@ import qualified Satchmo.Counting as C
 import qualified Satchmo.SAT.Mini
 import qualified Data.Map as M
 import Control.Monad ( forM, forM_, void, when )
+import System.Environment ( getArgs )
+
+main = do
+    [ d ] <- getArgs
+    run $ read d
 
 run :: Int -> IO ()
 run d = do
     Just out <- Satchmo.SAT.Mini.solve $ do
         funs @ [ plus, times, exp ] <- 
             sequence $ replicate 3 $ mkfun d 2
+        forM_ [ plus,times ] $ assert_symmetric 
+        -- forM_ [ plus,times ] $ assert_associative 
         let value k e = inter plus times exp d k e
         forM_ tarski $ \ (k, (l,r)) -> do
             ll <- value k l ; rr <- value k r
-            ok <- equals ll rr
-            B.assert [ ok ]
+            assert_equals ll rr
+
         let (k,(l,r)) = wilkie
         ll <- value k l ; rr <- value k r
         ok <- equals ll rr
         B.assert [ B.not ok ]        
+
         return ( C.decode funs 
               :: Satchmo.SAT.Mini.SAT [Fun Bool] 
                )
-    print out
+    forM_ out ( print . fun2form )
     
+fun2form f 
+    = map ( \ (y:xs) -> (xs, y) )   
+    $ map fst $ filter snd 
+    $ M.toList $ tomap f
 
 tarski =
     let x = Proj 1 ; y = Proj 2 ; z = Proj 3 in
-    [ (2, (Apply Plus x y
-          , Apply Plus  y x) )
-    , (3, ( Apply Plus (Apply Plus x y) z 
+    [
+{-
+      (2, (Apply Plus x y , Apply Plus y x) )
+    , (2, (Apply Times x y , Apply Times  y x) )
+-}
+      (3, ( Apply Plus (Apply Plus x y) z 
           , Apply Plus x (Apply Plus  y z)))
 
-    , (2, (Apply Times x y
-          , Apply Times  y x) )
     , (3, ( Apply Times (Apply Times x y) z 
           , Apply Times x (Apply Times  y z)))
-
     , (1, (Apply Times x One, x ))
 
     , (1, (Apply Exp x One, x ))
@@ -74,20 +86,18 @@ wilkie  =
         x3 = Apply Times x2 x
         x4 = Apply Times x2 x2
         a = Apply Plus One x
-        b = foldr1 (Apply Plus) [ One, x, x2 ]
+        b = foldl1 (Apply Plus) [ One, x, x2 ]
         c = Apply Plus One x3
-        d = foldr1 (Apply Plus) [One, x2, x4 ]
+        d = foldl1 (Apply Plus) [One, x2, x4 ]
         form p q i o = Apply Exp 
           (Apply Plus (Apply Exp p i) (Apply Exp q i)) 
           o
         side i o = Apply Times 
            (form a b i o) (form c d o i)
-    in  (2, ( side x y , side y x ))
+    in  (2, ( side x y , side x y ))
 
 data Op = Plus | Times | Exp deriving Show
-data E = One | Proj Int
-       | Apply Op E E 
-    deriving Show
+data E = One | Proj Int | Apply Op E E deriving Show
 
 
 inter :: Fun B.Boolean
@@ -105,60 +115,82 @@ inter plus times exp d k e = case e of
             Plus -> plus ; Times -> times; Exp -> exp
           ) [x,y]
 
+arguments d k = sequence $ replicate (k+1) [1..d]
+
+tomap f = M.fromList 
+                  $ for (arguments (dom f)(arity f))
+                  $ \ (y:xs) -> (y:xs, contents f xs y)
+tofun m = \ xs y -> m M.! (y : xs)                    
+
 -- | representation on k-ary functions
--- on finite omain [ 1 .. dom ]:
+-- on finite domain [ 1 .. dom ]:
 -- m [y, x1, .., xk ] iff  y = f(x1,..,xk) .
 -- this includes the special case k=0 (constants).
 data Fun b = 
      Fun { dom :: Int, arity :: Int
-         , contents :: M.Map [ Int ] b
+         , contents :: [Int] -> Int -> b
          }
-    deriving Show
+
 
 apply :: Fun b -> [Int] -> Int -> b
-apply f xs y = contents f M.! ( y : xs )
+apply f xs y = contents f xs y
 
 instance C.Decode m b c 
     => C.Decode m (Fun b) (Fun c) where
         decode f = do
-            c <- C.decode $ contents f
-            return $ f { contents = c }
+            let m = tomap f
+            c <- C.decode $ m
+            return $ f { contents = tofun c }
 
 constant d k c = do
-    f <- mkfun d k
-    forM (M.toList $ contents f) $ \ ((y:xs),v) ->do
-        when (y == c) $
-            B.assert [ B.not $ apply f xs y, v ]
-    return f
+    t <- B.constant True ; f <- B.constant False
+    return $ Fun { dom = d, arity = k
+                 , contents = \ xs y -> 
+                    if c == y then t else f
+                 }
 
 -- note: leftmost is projection d k 1,
--- rightmost is projectino d k k
+-- rightmost is projection d k k
 projection d k i = do
-    f <- mkfun d k
-    forM_ (M.toList $ contents f ) $ \ ((y:xs), v) -> 
-        B.assert [ B.not $ apply f xs y, v ]
-    return f
+    t <- B.constant True ; f <- B.constant False
+    return $ Fun { dom = d, arity = k
+                 , contents = \ xs y -> 
+                    if xs !!(i-1) == y then t else f
+                 }
 
 mkfun :: B.MonadSAT m
       => Int -- ^ domain
       -> Int -- ^ arity
       -> m (Fun B.Boolean)
 mkfun d k = do
-    pairs <- 
-      forM (sequence $ replicate (k+1) [1..d]) $ \ i ->
+    pairs <- forM (arguments d k) $ \ i ->
         do v <- B.boolean; return (i, v)
-    let c = M.fromList pairs  
-        f = Fun { dom = d, arity = k , contents = c }
-    forM_ ( sequence $ replicate k [1..d] ) $ \ xs -> do
+    let m = M.fromList pairs  
+        f = Fun { dom = d, arity = k 
+                , contents = tofun m }
+    forM_ ( arguments d (k-1) ) $ \ xs -> do
         e <- C.exactly 1 $ for [1..d] $ \ y ->
             apply f xs y
         B.assert [e]
     return f
 
 equals f g = do
-    oks <- forM ( M.keys $ contents f ) $ \ (y:xs) ->
+    oks <- forM ( arguments (dom f)(arity f) ) $ \ (y:xs) ->
         B.equals2 (apply f xs y) (apply g xs y)
     B.and oks
+
+assert_equals f g = do
+    forM_ ( arguments (dom f)(arity f) ) $ \(y:xs) ->do
+        let l = apply f xs y ; r =  apply g xs y
+        B.assert [ l, B.not r ]
+        B.assert [ r, B.not l ]
+
+assert_symmetric f = do
+    forM_ ( arguments (dom f) 2 ) $ \ [y, a, b] -> do
+        let l = apply f [a,b] y ; r = apply f [b,a] y
+        B.assert [ l, B.not r ]
+        B.assert [ r, B.not l ]
+
 
 substitute :: B.MonadSAT m
            => Fun B.Boolean
