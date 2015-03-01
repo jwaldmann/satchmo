b3f77c3f2369028b339c5800d83be37638aaad55
Johannes Waldmann <waldmann@imn.htwk-leipzig.de>
2015-03-01 18:16:08 +0100

    extend API by standard functions

--- Satchmo/Map/Data.hs
+++ Satchmo/Map/Data.hs
@@ -5,11 +5,13 @@ module Satchmo.Map.Data
 
 ( Map
 , unknown, constant
-, (!), 
+, (!), elems, keys, toList, fromList
+, map, mapWithKey
 ) 
 
 where
 
+import qualified Prelude; import Prelude hiding ( map ) 
 import Satchmo.Code
 import qualified Satchmo.Boolean as B
 
@@ -24,6 +26,12 @@ import Control.Applicative ( (<$>), (<*>) )
 newtype Map a b = Map (M.Map a b)
 
 Map m ! i = m M.! i
+elems (Map m) = M.elems m
+keys (Map m) = M.keys m
+toList (Map m) = M.toList m
+fromList kvs = Map $ M.fromList kvs
+map f (Map m) = Map (M.map f m)
+mapWithKey f (Map m) = Map (M.mapWithKey f m)
 
 instance ( Functor m, Decode m b c, Ord a )
          => Decode m (Map a b) ( M.Map a c) where
