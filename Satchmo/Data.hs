e2d19b2c9d733cf19f9b7e5c7a14b82fb83c89f6
Johannes Waldmann <waldmann@imn.htwk-leipzig.de>
2015-03-19 20:03:47 +0100

    change [] to Seq

--- Satchmo/Data.hs
+++ Satchmo/Data.hs
@@ -3,22 +3,28 @@
 module Satchmo.Data 
 
 ( CNF, cnf, clauses
--- FIXME: exports should be abstract
-, Clause(..), clause, literals
-, Literal (..), literal, nicht, positive, variable
+, Clause, clause, literals
+, Literal, literal, nicht, positive, variable
 , Variable 
 )
 
 where
 
-import Control.Monad.State.Strict
+import qualified Data.Sequence as S
+import Data.Foldable (toList)
+import Data.Monoid
+
 
 type Variable = Int
 
-data Literal = Literal { variable :: Variable , positive :: Bool }
+data Literal =
+     Literal { variable :: ! Variable
+             , positive :: ! Bool
+             }
 
 instance Show Literal where
-    show l = ( if positive l then "" else "-" ) ++ show ( variable l )
+    show l = ( if positive l then "" else "-" )
+             ++ show ( variable l )
 
 literal :: Bool -> Variable -> Literal
 literal pos v  = Literal { positive = pos, variable = v }
@@ -26,14 +32,18 @@ literal pos v  = Literal { positive = pos, variable = v }
 nicht :: Literal -> Literal 
 nicht x = x { positive = not $ positive x }
 
-newtype CNF     = CNF { clauses :: [ Clause ] }
+newtype CNF  = CNF ( S.Seq Clause )
+clauses (CNF s) = Data.Foldable.toList s
 
-instance Show ( CNF  ) where
-    show ( CNF cs ) = unlines $ map show cs
+instance Show CNF  where
+    show cnf = unlines $ map show $ clauses cnf
 
 cnf :: [ Clause ] -> CNF 
-cnf cs = CNF cs
+cnf cs = CNF $ S.fromList cs
 
+instance Monoid CNF where
+  mempty = CNF S.empty
+  mappend (CNF c1) (CNF c2) = CNF $ mappend c1 c2
 
 newtype Clause = Clause { literals :: [ Literal ] }
 
