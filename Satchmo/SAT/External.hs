9b6f19f627d100fb73f6e85ef2f815c9d7585bcd
Johannes Waldmann <waldmann@imn.htwk-leipzig.de>
2015-07-17 16:58:35 +0200

    version (fix deadlock)

--- Satchmo/SAT/External.hs
+++ Satchmo/SAT/External.hs
@@ -23,18 +23,34 @@ where
 import Satchmo.Data
 import Satchmo.Boolean hiding ( not )
 import Satchmo.Code
-import Satchmo.MonadSAT
+-- import Satchmo.MonadSAT
 
 import Control.Monad.Reader
 import Control.Monad.State
+-- import Control.Monad.IO.Class
 import System.IO
 import Control.Lens
 import Control.Applicative
 
+import Control.Concurrent
+import Control.DeepSeq (rnf)
+
+import Foreign.C
+-- import System.Exit (ExitCode(..))
 import System.Process
-import Control.Exception (bracket)
+-- import System.IO.Error
+-- import System.Posix.Types
+import Control.Exception
+import GHC.IO.Exception ( IOErrorType(..), IOException(..) )
+-- import System.Posix.Signals
+
+import qualified Control.Exception as C
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.Map.Strict as M
+import Data.List (isPrefixOf)
+
+tracing = False
+report s = when tracing $ hPutStrLn stderr s
 
 data S = S
        { _next_variable :: ! Int 
@@ -44,12 +60,12 @@ data S = S
 $(makeLenses ''S)
 
 newtype SAT a = SAT (StateT S IO a)
-  deriving (Functor, Applicative, Monad)
+  deriving (Functor, Applicative, Monad, MonadIO)
 
 type Assignment = M.Map Int Bool
 
 newtype Dec a = Dec (Reader Assignment a)
-  deriving Monad  
+  deriving (Functor, Applicative, Monad)
 
 instance MonadSAT SAT where
   fresh = SAT $ do 
@@ -79,8 +95,6 @@ dv v = Dec $ do
     Just v -> v
     Nothing -> error $ unwords [ "unassigned", "variable", show v ]
       
-tracing = False
-report s = when tracing $ hPutStrLn stderr s
 
 solve :: String  -- ^ command, e.g., glucose
       -> [String] -- ^ options, e.g., -model
@@ -99,26 +113,67 @@ solve command opts (SAT action) = bracket
        interruptProcessGroupOf ph
    )
    $ \ (Just sin, Just sout, _, ph) -> do
-       let s0 = S { _next_variable=1, _solver_input=sin}
-       Dec decoder <- evalStateT action s0
-       hClose sin
+
+       dec <- newEmptyMVar
+
+       -- fork off a thread to start consuming the output
+       output  <- hGetContents sout -- lazy IO
+       withForkWait (C.evaluate $ rnf output) $ \ waitOut -> 
+          ignoreSigPipe $ do
+            report $ "S.S.External: waiter forked"
+
+            let s0 = S { _next_variable=1, _solver_input=sin}
+            report $ "S.S.External: writing output"
+            Dec decoder <- evalStateT action s0
+            putMVar dec decoder
+            hClose sin
+
+            waitOut
+            hClose sout
+            report $ "S.S.External: waiter done"
+
        report "Satchmo.SAT.External: start waiting"
        waitForProcess ph
+       decoder <- takeMVar dec
        report "Satchmo.SAT.External: waiting done"
-       out <- liftIO $ BS.hGetContents sout
+
        let vlines = do
-             line <- BS.lines out
-             guard $ BS.isPrefixOf (BS.pack "v") line
+             line <- lines output
+             guard $ isPrefixOf "v" line
              return line
        report $ show vlines
        let vs = do
              line <- vlines
-             w <- tail $ BS.words line
-             return (read $ BS.unpack w :: Int)
+             w <- tail $ words line
+             return (read w :: Int)
        return $ do
          guard $ not $ null vlines
          let m = M.fromList $ do 
                v <- vs ; guard $ v /= 0 ; return (abs v, v>0)
          return $ runReader decoder m
 
-  
+-- * code from System.Process 
+-- http://hackage.haskell.org/package/process-1.2.3.0/docs/src/System-Process.html#readProcess
+-- but they are not exporting withForkWait, so I have to copy it
+
+-- | Fork a thread while doing something else, but kill it if there's an
+-- exception.
+--
+-- This is important in the cases above because we want to kill the thread
+-- that is holding the Handle lock, because when we clean up the process we
+-- try to close that handle, which could otherwise deadlock.
+--
+withForkWait :: IO () -> (IO () ->  IO a) -> IO a
+withForkWait async body = do
+  waitVar <- newEmptyMVar :: IO (MVar (Either SomeException ()))
+  mask $ \restore -> do
+    tid <- forkIO $ try (restore async) >>= putMVar waitVar
+    let wait = takeMVar waitVar >>= either throwIO return
+    restore (body wait) `C.onException` killThread tid
+
+ignoreSigPipe :: IO () -> IO ()
+ignoreSigPipe = C.handle $ \e -> case e of
+  IOError { ioe_type  = ResourceVanished
+          , ioe_errno = Just ioe }
+    | Errno ioe == ePIPE -> return ()
+  _ -> throwIO e
